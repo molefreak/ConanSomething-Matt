93babcf93260367bb2c5cafdae68d16e7a1ab36e
molefreak <molefreak1992@hotmail.co.uk>
2014-02-28 23:04:00 +0000

    Player Movement
    player movement

---
+++ GameJam2/Assets/PlayerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	float speed =5.0f;
+
+	Vector3 movePosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetAxis("Horizontal")!=0){
+
+			movePosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+			transform.position = movePosition;
+		}
+
+		if(Input.GetAxis("Vertical")!=0){
+			
+			movePosition.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+			transform.position = movePosition;
+		}
+	}
+}
